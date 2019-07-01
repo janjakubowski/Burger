87790f38b2350c620dae8309246d2f4946af0f66
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-07-01 12:47:14 -0400

    added orm, the model and the controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+//
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+	burger.all(function(data) {
+		var handlebarsObject = {
+			burgers: data
+		};
+		console.log(handlebarsObject);
+		res.render("index", handlebarsObject);
+	});
+});
+
+// 
+// POST to add new burger, "devoured" default to "false"
+router.post("/api/burgers", function(req, res) {
+	burger.create([
+		"name"
+	], [
+		req.body.name
+	], function(result) {
+		res.json({ id: result.insertId });
+	});
+});
+
+// PUT to update "burger" equal to "id" to "true"
+router.put("/api/burgers/:id", function(req, res) {
+	var condition = "id = " + req.params.id;
+	burger.update({
+		devoured: "true"
+	}, condition, function(result) {
+		if (result.changedRows == 0) {
+			// If no rows were changed, then the ID must not exist, so 404
+			return res.status(404).end();
+		} else {
+			res.status(200).end();
+		}
+	});
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+	 if (result.affectedRows == 0) {
+		// row with ID does not exist, so return 404
+		return res.status(404).end();
+	 } else {
+		res.status(200).end();
+	 }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
