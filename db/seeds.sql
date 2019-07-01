f36b18fd01c046d4ccdabfa6e45626493f86c939
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-07-01 11:31:10 -0400

    add package.json, .gitignore, and database related: schema, seeds and connection.js, plus server.js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) 
+VALUES 	('Impossible Burger'),
+		('Beyond Beef Burger?'),
+		('Southwestern Turkey Burger'),
+		('Salmon Burger'),
+		('Where is the Beef Burger'),
+		('All American Burger with A Fried Egg');
+
